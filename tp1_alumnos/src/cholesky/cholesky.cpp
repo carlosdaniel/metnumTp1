d41e068b730dff8e7bb5d348fef2e41c5fab96f0
carlosdaniel <cdani.nm@gmail.com>
2017-04-12 16:06:31 -0300

    Update cholesky.cpp
    choleski andando

--- tp1_alumnos/src/cholesky/cholesky.cpp
+++ tp1_alumnos/src/cholesky/cholesky.cpp
@@ -5,13 +5,12 @@
 #include <sstream>
 #include <stdlib.h>
 #include <vector>
-#include <math.h> 
-
+#include <math.h>
 using namespace std;
 
 int main(){
 	ifstream file;
-    file.open("entrada.txt",ios::in);
+    file.open("input.txt",ios::in);
     string line;
 
     if (file.fail()){
@@ -20,7 +19,7 @@ int main(){
         string leoInt;
         getline(file, leoInt, ' ');
         int cantEq = atoi(leoInt.c_str());
-        double A[cantEq][cantEq];
+        float A[cantEq][cantEq];
         struct puntosEq {
             int pg; //Partidos ganados
             int pp; //partidos perdidos
@@ -61,7 +60,7 @@ int main(){
                 E[linea[3]-1].pg ++;
             }
         }
-        
+
         cout << "la matriz inicial es esta: " << endl;
         for(int i=0; i<cantEq ; i++){
 			for (int j=0; j<cantEq ; j++){
@@ -69,30 +68,30 @@ int main(){
 			}
 			cout << endl;
 		}
-        
-        double b[cantEq];
+
+        float b[cantEq];
         for (int i = 0; i<cantEq;i++){
             b[i] = 1 + (((float)E[i].pg - (float)E[i].pp)/2);
         }
-        
+
 		float acum;
 
 		//comienza el metodo de choleski
 		for(int k=0; k<cantEq; k++){
-			for(int i=0; i<=k-1; i++){
+			for(int i=0; i<k; i++){
 				acum = 0.0;
-				for(int j=0;j<=i-1;j++){
+				for(int j=0;j<i;j++){
 					acum = acum + (A[i][j]*A[k][j]);
 				}
-				A[k][i]=A[k][i]- (acum/A[i][i]);
+				A[k][i]=(A[k][i]-acum)/A[i][i];
 			}
 			acum = 0.0;
-			for (int j=0;j<=k-1;j++){
+			for (int j=0;j<k;j++){
 				acum = acum + A[k][j]*A[k][j];
 			}
 			A[k][k] = sqrt(A[k][k]-acum);
 		}
-		
+
 		cout << "la matriz despues de cholesky es la sgte:" << endl;
 		for(int i=0;i<cantEq;i++){
 			for(int j=0;j<cantEq;j++){
@@ -100,21 +99,29 @@ int main(){
 			}
 			cout << endl;
 		}
-		//como A es triangular inferior y Lt es es triangular superior 		
+		//como A es triangular inferior y Lt es es triangular superior
 		//vamos a unir las dos matrices en A, de manera que A queda simetrica.
-		
-		for(int i=1; i<cantEq; i++){ 
-			for(int j=0;j<i;j++){ 
+
+		for(int i=1; i<cantEq; i++){
+			for(int j=0;j<i;j++){
 				A[j][i] = A[i][j];
 			}
 		}
-		//La matriz A quedó convertida en L, luego Lt es solo intercambiar filas por columnas
+
+        cout << "la matriz compuesta de L(interseccion) Lt es la sgte:" << endl;
+		for(int i=0;i<cantEq;i++){
+			for(int j=0;j<cantEq;j++){
+				cout << "|" << A[i][j] << "|";
+			}
+			cout << endl;
+		}
+        //La matriz A quedó convertida en L, luego Lt es solo intercambiar filas por columnas
         //La idea de cholesky es la siguiente:
         //Ax = b ......   A=L*Lt
         //Ax = b es lo mismo que resolver L * Lt*x = b, tenemos L vamos a usar Lt*x = y
         //resolvemos L * y = b
-        
-        double y[cantEq];
+
+        float y[cantEq];
         y[0]=b[0]/A[0][0];
         //vamos armando el vector Y progresivamente A es triangular inferior
         for(int i=1;i<cantEq;i++){
@@ -124,17 +131,23 @@ int main(){
 			}
 			y[i]=(b[i]-acum)/A[i][i];
 		}
-				
-		//ya se tiene el vector y, lo que falta realizar es Lt *x = y 
-		double x[cantEq];
+
+		//ya se tiene el vector y, lo que falta realizar es Lt *x = y
+		float x[cantEq];
 		x[cantEq]=y[cantEq-1]/A[cantEq-1][cantEq-1];
-		
+
 		for(int i=cantEq-2;i>=0;i--){
 			acum = 0.0;
 			for(int j=cantEq-1;j>i;j--){
 				acum = acum + A[i][j]*x[j];
 			}
 			x[i]=(y[i]-acum)/A[i][i];
+		}
+		//resultado del problema
+		cout << "el resultado del algoritmo es el siguiente :" << endl;
+		cout << " equipo -- resultado" << endl;
+		for(int i=0;i<cantEq;i++){
+            cout << i+1 << " -- " << x[i] << endl;
 		}
 
 	}
