26c1175fef949048547ca2945a6ae91d1aa659db
carlosdaniel <cdani.nm@gmail.com>
2017-04-12 11:26:25 -0300

    Update cholesky.cpp
    les voy pasando el cpp, falta corregir el archivo

--- tp1_alumnos/src/cholesky/cholesky.cpp
+++ tp1_alumnos/src/cholesky/cholesky.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 int main(){
 	ifstream file;
-    file.open("input.txt",ios::in);
+    file.open("entrada.txt",ios::in);
     string line;
 
     if (file.fail()){
@@ -20,14 +20,14 @@ int main(){
         string leoInt;
         getline(file, leoInt, ' ');
         int cantEq = atoi(leoInt.c_str());
-        float A[cantEq][cantEq];
+        double A[cantEq][cantEq];
         struct puntosEq {
             int pg; //Partidos ganados
             int pp; //partidos perdidos
         } E [cantEq];
         getline(file, leoInt);
         int cantPartidos = atoi(leoInt.c_str());
-        int linea[4];
+        int linea[5];
         string num;
         for(int i = 0; i< cantEq;i++){
             for(int j = 0; j< cantEq;j++){
@@ -47,21 +47,30 @@ int main(){
                 linea[i] = atoi(num.c_str());
                 i++;
             }
-            A[linea[0]-1][linea[2]-1] --;
-            A[linea[2]-1][linea[0]-1] --;
-            A[linea[0]-1][linea[0]-1] ++;
-            A[linea[2]-1][linea[2]-1] ++;
-            if(linea[1] > linea[3]){
+            A[linea[1]-1][linea[3]-1] --;
+            A[linea[3]-1][linea[1]-1] --;
+            A[linea[1]-1][linea[1]-1] ++;
+            A[linea[3]-1][linea[3]-1] ++;
+            if(linea[2] > linea[4]){
                 //Gano equipo 1
-                E[linea[0]-1].pg ++;
-                E[linea[2]-1].pp ++;
+                E[linea[1]-1].pg ++;
+                E[linea[3]-1].pp ++;
 
             }else{
-                E[linea[0]-1].pp ++;
-                E[linea[2]-1].pg ++;
+                E[linea[1]-1].pp ++;
+                E[linea[3]-1].pg ++;
             }
         }
-        float b[cantEq];
+        
+        cout << "la matriz inicial es esta: " << endl;
+        for(int i=0; i<cantEq ; i++){
+			for (int j=0; j<cantEq ; j++){
+			cout << "|" << A[i][j] << "|";
+			}
+			cout << endl;
+		}
+        
+        double b[cantEq];
         for (int i = 0; i<cantEq;i++){
             b[i] = 1 + (((float)E[i].pg - (float)E[i].pp)/2);
         }
@@ -84,26 +93,50 @@ int main(){
 			A[k][k] = sqrt(A[k][k]-acum);
 		}
 		
-		//declaramos la matriz traspuesta
-		float Lt[cantEq][cantEq];
+		cout << "la matriz despues de cholesky es la sgte:" << endl;
+		for(int i=0;i<cantEq;i++){
+			for(int j=0;j<cantEq;j++){
+				cout << "|" << A[i][j] << "|";
+			}
+			cout << endl;
+		}
+		//como A es triangular inferior y Lt es es triangular superior 		
+		//vamos a unir las dos matrices en A, de manera que A queda simetrica.
 		
-		//añadimos los ceros por arriba de la diagonal de A
-		//añadimos los ceros por debajo de la diagonal de Lt
-		//copiamos espejdamente los valores de A a Lt
-		for(int i=0; i<cantEq; i++){ 
-			for(int j=0;j<cantEq;j++){ 
-				if (j>i){
-					A[i][j] = 0.0;
-					Lt[j][i] = 0.0;
-				}else{
-					Lt[j][i]= A[i][j];
-				}
+		for(int i=1; i<cantEq; i++){ 
+			for(int j=0;j<i;j++){ 
+				A[j][i] = A[i][j];
 			}
 		}
 		//La matriz A quedó convertida en L, luego Lt es solo intercambiar filas por columnas
+        //La idea de cholesky es la siguiente:
+        //Ax = b ......   A=L*Lt
+        //Ax = b es lo mismo que resolver L * Lt*x = b, tenemos L vamos a usar Lt*x = y
+        //resolvemos L * y = b
         
-        
-		//falta la resolución
+        double y[cantEq];
+        y[0]=b[0]/A[0][0];
+        //vamos armando el vector Y progresivamente A es triangular inferior
+        for(int i=1;i<cantEq;i++){
+			acum = 0.0;
+			for(int j=0;j<i;j++){
+				acum = acum + (A[i][j]*y[j]);
+			}
+			y[i]=(b[i]-acum)/A[i][i];
+		}
+				
+		//ya se tiene el vector y, lo que falta realizar es Lt *x = y 
+		double x[cantEq];
+		x[cantEq]=y[cantEq-1]/A[cantEq-1][cantEq-1];
+		
+		for(int i=cantEq-2;i>=0;i--){
+			acum = 0.0;
+			for(int j=cantEq-1;j>i;j--){
+				acum = acum + A[i][j]*x[j];
+			}
+			x[i]=(y[i]-acum)/A[i][i];
+		}
+
 	}
 return 0;
 }
